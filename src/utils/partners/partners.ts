29def952458e13907a49a16bac89ff6d9dbef8b1
Ubirajara Neves <biraneves@outlook.com>
2024-02-15 09:00:22 -0300

    Partners array

---
+++ src/utils/partners/partners.ts
@@ -0,0 +1,119 @@
+type Partner = {
+  name: string;
+  logo: string;
+};
+
+export const partners: Partner[] = [
+  {
+    name: 'Abbott',
+    logo: '/assets/images/partners/abbott-logo.png',
+  },
+  {
+    name: 'Atlas',
+    logo: '/assets/images/partners/atlas-logo.png',
+  },
+  {
+    name: 'AT&T',
+    logo: '/assets/images/partners/att-logo.png',
+  },
+  {
+    name: 'Cisco',
+    logo: '/assets/images/partners/cisco-logo.png',
+  },
+  {
+    name: 'Coca-Cola',
+    logo: '/assets/images/partners/coca-cola-logo.png',
+  },
+  {
+    name: 'Distree Events',
+    logo: '/assets/images/partners/distree-logo.png',
+  },
+  {
+    name: 'EMS',
+    logo: '/assets/images/partners/ems-logo.png',
+  },
+  {
+    name: 'ESADE Business School',
+    logo: '/assets/images/partners/esade-logo.png',
+  },
+  {
+    name: 'Eucatex',
+    logo: '/assets/images/partners/eucatex-logo.png',
+  },
+  {
+    name: 'Evonik',
+    logo: '/assets/images/partners/evonik-logo.png',
+  },
+  {
+    name: 'FedEx',
+    logo: '/assets/images/partners/fedex-logo.png',
+  },
+  {
+    name: 'Germed',
+    logo: '/assets/images/partners/germed-logo.png',
+  },
+  {
+    name: 'GSK',
+    logo: '/assets/images/partners/gsk-logo.png',
+  },
+  {
+    name: 'Johnson & Johnson',
+    logo: '/assets/images/partners/johnson-logo.png',
+  },
+  {
+    name: 'Mapfre',
+    logo: '/assets/images/partners/mapfre-logo.png',
+  },
+  {
+    name: 'Match Connection',
+    logo: '/assets/images/partners/match-logo.png',
+  },
+  {
+    name: 'Merck',
+    logo: '/assets/images/partners/merck-logo.png',
+  },
+  {
+    name: 'Novartis',
+    logo: '/assets/images/partners/novartis-logo.png',
+  },
+  {
+    name: 'PCI',
+    logo: '/assets/images/partners/pci-logo.png',
+  },
+  {
+    name: 'Pfizer',
+    logo: '/assets/images/partners/pfizer-logo.png',
+  },
+  {
+    name: 'Poker Stars',
+    logo: 'poker-stars-logo.png',
+  },
+  {
+    name: 'Rabobank',
+    logo: 'rabobank-logo.png',
+  },
+  {
+    name: 'Roche',
+    logo: 'roche-logo.png',
+  },
+  {
+    name: 'Samsung',
+    logo: 'samsung-logo.png',
+  },
+  {
+    name: 'Santander',
+    logo: 'santander-logo.png',
+  },
+  {
+    name: 'Scania',
+    logo: 'scania-logo.png',
+  },
+  {
+    name: 'Telefonica',
+    logo: 'telefonica-logo.png',
+  },
+  {
+    name: 'Viajes El Corte Inglés',
+    logo: 'viajes-el-corte-ingles-logo.png',
+  },
+];
