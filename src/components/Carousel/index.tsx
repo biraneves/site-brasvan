27a979255fae1a0978dbe262566acae58cfafc95
Ubirajara Neves <biraneves@outlook.com>
2024-02-15 09:08:02 -0300

    Basic structure of Carousel component

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Partner } from '../../utils/partners/partners';
+
+const Carousel = (partners: Partner[]) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstImage = currentIndex === 0;
+    const newIndex = isFirstImage ? partners.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastImage = currentIndex === partners.length - 1;
+    const newIndex = isLastImage ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div>
+      <img
+        src={partners[currentIndex].logo}
+        alt={partners[currentIndex].name}
+      />
+      <button onClick={goToPrevious}>Anterior</button>
+      <button onClick={goToNext}>Próximo</button>
+    </div>
+  );
+};
+
+export default Carousel;
