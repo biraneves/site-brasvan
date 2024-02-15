7c1dd6786049e2a30bd176513a4c43087986062e
Ubirajara Neves <biraneves@outlook.com>
2024-02-15 12:25:25 -0300

    Scroll with offset

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import { translations } from '../../translations';
 import en from '../../assets/images/languages/en.png';
 import es from '../../assets/images/languages/es.png';
 import pt from '../../assets/images/languages/pt.png';
+import { useEffect, useRef, useState } from 'react';
 
 type LanguageImages = {
   [key: string]: string;
@@ -17,39 +18,69 @@ const Header = ({ language, alterLanguage }: IProps) => {
     pt: pt,
   };
 
+  const [height, setHeight] = useState(0);
+  const elementRef = useRef<HTMLHeadElement>(null);
+
+  useEffect(() => {
+    if (elementRef.current) {
+      setHeight(elementRef.current.offsetHeight);
+    }
+  }, []);
+
+  const scrollWithOffset = (el: HTMLElement) => {
+    const yCoordinate = el.getBoundingClientRect().top + window.scrollY;
+    const yOffset = -height;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
+  };
+
   return (
-    <HeaderContainer className="glass">
+    <HeaderContainer ref={elementRef}>
       <div className="container">
         <img src={logo} alt="Brasvan Viagens e Turismo" />
         <nav>
           <LinksContainer>
             <li>
-              <LinkContainer to="#hero">
+              <LinkContainer to="#hero" scroll={(el) => scrollWithOffset(el)}>
                 {translations[language].header.hdrHome}
               </LinkContainer>
             </li>
             <li>
-              <LinkContainer to="#about-us">
+              <LinkContainer
+                to="#about-us"
+                scroll={(el) => scrollWithOffset(el)}
+              >
                 {translations[language].header.hdrAboutUs}
               </LinkContainer>
             </li>
             <li>
-              <LinkContainer to="#services">
+              <LinkContainer
+                to="#services"
+                scroll={(el) => scrollWithOffset(el)}
+              >
                 {translations[language].header.hdrServices}
               </LinkContainer>
             </li>
             <li>
-              <LinkContainer to="#vehicles">
+              <LinkContainer
+                to="#vehicles"
+                scroll={(el) => scrollWithOffset(el)}
+              >
                 {translations[language].header.hdrVehicles}
               </LinkContainer>
             </li>
             <li>
-              <LinkContainer to="#customers">
+              <LinkContainer
+                to="#customers"
+                scroll={(el) => scrollWithOffset(el)}
+              >
                 {translations[language].header.hdrCustomers}
               </LinkContainer>
             </li>
             <li>
-              <LinkContainer to="#contact">
+              <LinkContainer
+                to="#contact"
+                scroll={(el) => scrollWithOffset(el)}
+              >
                 {translations[language].header.hdrContact}
               </LinkContainer>
             </li>
